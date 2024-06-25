cd42f387b81b3528858114ca86c776edbb82eec0
devexplorer <contact@devexplorer.fr>
2024-06-25 13:48:48 -0400

    new function for create csv with books data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests as req
 import product
+import csv
 
 from bs4 import BeautifulSoup
 
@@ -10,19 +11,29 @@ def parse_data(url):
     return soup
 
 
+def create_csv(data_products):
+    with open('products.csv', 'w', newline='') as csvfile:
+        fieldnames = data_products[0].keys()
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for line_product in data_products:
+            writer.writerow(line_product)
+
+
 def get_data_article_by_href(url, href):
-    data = parse_data(url + href)
-    test = product.get_info_product(data)
-    print(test)
+    data_parse = parse_data(url + href)
+    return product.get_info_product(data_parse)
 
 
 def get_data_page(url):
+    list_products = []
     data = parse_data(url)
 
     for i, article in enumerate(data.find_all('article')):
-        if i == 1:
-            get_data_article_by_href(url, article.a['href'])
+        list_products.append(get_data_article_by_href(url, article.a['href']))
+
+        create_csv(list_products)
 
 
-get_data_page('https://books.toscrape.com/')
+get_data_page('https://books.toscrape.com/', data={'key': 'value'})
 
--- product.py
+++ product.py
@@ -21,7 +21,7 @@ def get_info_product(data):
         "title": data.h1.string,
         "price": data.find(class_="price_color").string,
         "star_rating": get_number_star(data.find(class_="star-rating")),
-        "description": data.find(id="product_description").find_next_sibling().string,
+        # "description": data.find(id="product_description").find_next_sibling().string,
         "upc": data.find_all('td')[0].string,
         "product_type": data.find_all('td')[1].string,
         "price_excl_tax": data.find_all('td')[2].string,
